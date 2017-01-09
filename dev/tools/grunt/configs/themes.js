07559c3b5485c4a69a79b0e063e0c310a3e4a5b1
Kuldeepsingh Jeewoololl <kjeewoololl@kjeewoololl.local>
2017-01-09 11:01:24 -0500

    switched to less + fixed CSS overrides

--- dev/tools/grunt/configs/themes.js
+++ dev/tools/grunt/configs/themes.js
@@ -36,7 +36,7 @@ module.exports = {
         locale: 'en_US',
         files: [
             'css/bootstrap',
-            'css/style'
+            'css/main'
         ],
         dsl: 'less'
     },
