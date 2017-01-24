19669b8c753cbf1599d744513f5c17367f123b54
Kuldeepsingh Jeewoololl <anoop.jeewoolall@gmail.com>
2017-01-23 19:54:30 -0500

    added active to submenu to expand

--- app/design/frontend/Shop/hh/web/js/main.js
+++ app/design/frontend/Shop/hh/web/js/main.js
@@ -3,6 +3,7 @@ require(['jquery', 'jquery.bootstrap'], function($){
     $(function(){
         // This function is needed (even if empty) to force RequireJS to load Twitter Bootstrap and its Data API.
 
-        console.log('loaded');
+        console.log('Loaded Js!');
+        $('.level2.active').parent().addClass('display-block');
     });
 });
